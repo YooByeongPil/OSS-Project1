ad748a5c8b31e2360b1dfe16c4bbe0d94b327981
JoBumHee <afsdzvcx123@gmail.com>
2016-10-24 17:05:21 +0900

    학번추가(조범희)

--- main2.c
+++ main2.c
@@ -5,5 +5,8 @@ int main(void)
   printf("Open Source SW Project\n");
   printf("first GitHub Tranning\n");
   printf("BumHee We Finish\n");
+  
+  printf("2012244010 YooByeongPil");
+  printf("2012244055 JoBumHee");
   return 0;
 }
